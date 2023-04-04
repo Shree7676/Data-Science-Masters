2c0ff7e5412da8485ab8d4a062d221d4f3338efb
Shree7676 <shreekantnandiyawar@gmail.com>
2023-04-05 02:10:51 +0530

    5 assignment completed

--- Flask_basics/app.py
+++ Flask_basics/app.py
@@ -2,18 +2,18 @@ from flask import Flask, render_template, request, jsonify
 
 app=Flask(__name__)
 
-@app.route("/")
-def check():
-    return render_template("index.html")
+# @app.route("/")
+# def check():
+#     return render_template("index.html")
 
-@app.route("/shree_main",methods=['POST'])
-def get_data():
-    if (request.method=="POST"):
-        un=request.form['un']
-        pw=request.form['pw']
-        data="your username is {} and your password is {}".format(un,pw)
+# @app.route("/shree_main",methods=['POST'])
+# def get_data():
+#     if (request.method=="POST"):
+#         un=request.form['un']
+#         pw=request.form['pw']
+#         data="your username is {} and your password is {}".format(un,pw)
 
-    return render_template("results.html",result=data)
+#     return render_template("results.html",result=data)
 
 @app.route("/through_postman",methods=['POST'])
 def get_data_through_postman():
